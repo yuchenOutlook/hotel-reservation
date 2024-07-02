2e6a530e913c9496f2ce945f34bdd62b5fbf4790
VincentXiao <xiaoyuchen58@outlook.com>
2024-07-02 15:15:32 +0800

    feat: Add user API handlers for getting users and user by ID

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/yuchenOutlook/hotel-reservation/api"
 )
 
 
@@ -14,15 +15,7 @@ func main() {
 	app := fiber.New()
 	apiv1 := app.Group("api/v1")
 
-	app.Get("/foo", handleFoo)
-	apiv1.Get("/user", handleUser)
+	apiv1.Get("/user", api.HandleGetUsers)
+	apiv1.Get("/user/:id", api.HandleGetUser)
 	app.Listen(*listenAddr)
-}
-
-func handleFoo(c *fiber.Ctx) error {
-	return c.JSON(map[string]string {"msg": "working just fine!"})
-}
-
-func handleUser(c *fiber.Ctx) error {
-	return c.JSON(map[string]string {"user": "James Bond"})
 }
