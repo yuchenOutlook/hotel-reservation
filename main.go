9a1fe435ed2214b8ee042e5f84941d15c3f6a37e
VincentXiao <xiaoyuchen58@outlook.com>
2024-07-02 14:55:17 +0800

    feat: Add flag for customizable listen address in main.go

--- main.go
+++ main.go
@@ -1,14 +1,28 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"flag"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 
 func main() {
+	listenAddr := flag.String("listenAddr", ":5000", "The listen address of the API server")
+	// This parses the command line args from [1:]
+	flag.Parse()
 	app := fiber.New()
+	apiv1 := app.Group("api/v1")
+
 	app.Get("/foo", handleFoo)
-	app.Listen(":5000")
+	apiv1.Get("/user", handleUser)
+	app.Listen(*listenAddr)
 }
 
 func handleFoo(c *fiber.Ctx) error {
 	return c.JSON(map[string]string {"msg": "working just fine!"})
+}
+
+func handleUser(c *fiber.Ctx) error {
+	return c.JSON(map[string]string {"user": "James Bond"})
 }
